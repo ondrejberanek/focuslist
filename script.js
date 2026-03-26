9f7ac7a141fca2483d7f49f491889c37a305b175
OndrejBeranek <ondrovadir@gmil.com>
2026-03-26 17:32:53 +0100

    pridani funkce pro pridani ukolu

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("App běží");
+const input = document.getElementById("taskInput");
+const button = document.getElementById("addTaskBtn");
+const list = document.getElementById("taskList");
+
+button.addEventListener("click", function() {
+
+    const text = input.value;
+
+    if(text === "") return;
+
+    const li = document.createElement("li");
+    li.textContent = text;
+
+    list.appendChild(li);
+
+    input.value = "";
+
+});
